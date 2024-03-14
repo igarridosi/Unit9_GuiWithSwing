341b1c4da87352f2bec37d15d5770fced7e93ee1
Ibai Garrido <igarridosi23wg@ikzubirimanteo.com>
2024-03-14 22:54:43 +0100

    Add check box in the border layout

--- Mile1.java
+++ Mile1.java
@@ -1,8 +1,25 @@
 package Milestone1;
 
-public class Mile1 {
-    public Mile1(){
+import javax.swing.*;
+import java.awt.*;
 
+public class Mile1 extends JFrame {
+    public Mile1(){
+        super("Try yourself");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setPreferredSize(new Dimension(900,500));
+
+        Container panel = getContentPane();
+
+        panel.setLayout(new BorderLayout());
+
+        panel.add(new JCheckBox("Katniss"), BorderLayout.NORTH);
+        panel.add(new JCheckBox("Peeta"), BorderLayout.SOUTH);
+
+        //https://www.geeksforgeeks.org/java-awt-borderlayout-class/
+
+        this.pack();
+        this.setVisible(true);
     }
 
 
