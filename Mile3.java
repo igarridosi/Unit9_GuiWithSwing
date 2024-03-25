0024055620f8d98129598d7a4f9ce42e4c9dceb2
Ibai Garrido <igarridosi23wg@ikzubirimanteo.com>
2024-03-25 17:55:50 +0100

    Add the JOptionPane for the password

--- Mile3.java
+++ Mile3.java
@@ -2,6 +2,7 @@ package Milestones;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class Mile3 extends JFrame {
     public Mile3(){
@@ -9,8 +10,27 @@ public class Mile3 extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setPreferredSize(new Dimension(900,500));
 
+        //Password check
+        JPasswordField passwordField = new JPasswordField();
+        Object[] message = {"Enter Password:", passwordField};
 
+        int option = JOptionPane.showConfirmDialog(null, message, "Password Prompt", JOptionPane.OK_CANCEL_OPTION);
 
+        if (option == JOptionPane.OK_OPTION) {
+            char[] password = passwordField.getPassword();
+            String enteredPassword = new String(password);
+
+            if (enteredPassword.equals("damocles")) {
+                System.out.println("Entered Password: " + enteredPassword);
+            }
+            else{
+                System.out.println("Wrong pass");
+                System.exit(0);
+            }
+        }
+        else {
+            System.out.println("Canceled the operation.");
+        }
 
         this.setVisible(true);
         this.pack();
