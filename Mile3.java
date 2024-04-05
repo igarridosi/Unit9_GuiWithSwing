fc893f90701be9ef744597eddefd062d110c4a5f
Ibai Garrido <igarridosi23wg@ikzubirimanteo.com>
2024-04-05 17:01:21 +0200

    Adding plenty of changes to start handling events

--- Mile3.java
+++ Mile3.java
@@ -2,12 +2,19 @@ package Milestones;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Arrays;
 
-public class Mile3 extends JFrame {
+public class Mile3 extends JFrame{
     private Container container;
     private JPanel panel;
+    private JLabel label;
     private JComboBox combo;
+    private ImageIcon imageIcon;
+    private JCheckBox checkBox;
+    private JTextField textField;
+    private JButton saveButton;
 
     public Mile3(){
         super("Load Image");
@@ -45,9 +52,6 @@ public class Mile3 extends JFrame {
 
         //DESIGN OF THE WINDOW
 
-        //Add the container
-        container = getContentPane();
-
         //Add the ComboBox
         String [] images = {"dragon.jpg", "garden.jpg", "shop.jpg"};
         combo = new JComboBox<>(images);
@@ -56,7 +60,34 @@ public class Mile3 extends JFrame {
         combo.setBounds(20, 20, 200, 45);
 
         panel.add(combo);
+        System.out.println(combo.getSelectedIndex());
 
+
+
+
+
+
+
+
+
+
+
+
+        //Add check box
+        checkBox = new JCheckBox("Save your comment");
+        checkBox.setBounds(50, 300, 200,50);
+        checkBox.isSelected();
+        panel.add(checkBox);
+
+        //Add the Text Area
+        textField = new JTextField(10);
+        textField.setBounds(310, 300, 250, 40);
+        panel.add(textField);
+
+        //Add save Button
+        saveButton = new JButton("SAVE");
+        saveButton.setBounds(200, 400, 100, 40);
+        panel.add(saveButton);
 
         this.setVisible(true);
         this.pack();
@@ -64,5 +95,29 @@ public class Mile3 extends JFrame {
 
     public static void main(String[] args) {
         new Mile3();
+    }
+
+    public void load_combo(){
+        int selectedIndex = combo.getSelectedIndex();
+
+        //Load the image based on the selected index
+        ImageIcon imageIcon;
+        switch (selectedIndex) {
+            case 0:
+                imageIcon = new ImageIcon("Mile3_images/dragon.jpg");
+                break;
+            case 1:
+                imageIcon = new ImageIcon("Mile3_images/garden.jpg");
+                break;
+            case 2:
+                imageIcon = new ImageIcon("Mile3_images/shop.jpg");
+                break;
+            default:
+                imageIcon = null;
+        }
+
+        if (imageIcon != null) {
+            panel.add(new JLabel(imageIcon));
+        }
     }
 }
