5aa95c27d128b7bd1e02dcf7f87523ff9ce056fc
Ibai Garrido <igarridosi23wg@ikzubirimanteo.com>
2024-04-23 21:43:29 +0200

    Trying the mouseLister into the JList for displaying the selected image

--- Mile4.java
+++ Mile4.java
@@ -3,14 +3,14 @@ package Milestones;
 import org.jdesktop.swingx.JXDatePicker;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-public class Mile4 extends JFrame {
+public class Mile4 extends JFrame{
     private Container container;
     private JPanel panel1, panel2, panel3, panel4;
     private JComboBox<String> combo;
@@ -28,15 +28,38 @@ public class Mile4 extends JFrame {
         container = getContentPane();
         container.setLayout(new GridLayout(2,2));
 
+//////////Combo Names//////////////////////////////////
+
         //Adding ComboBox to panel1
         panel1 = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
         label1 = new JLabel("Photographer: ");
 
+        //Array for changing the values of the jList
         String [] photographerNames = photographerNames();
-
         combo = new JComboBox<>(photographerNames);
         combo.setSize(100, 40);
+
+        panel1.add(label1);
+        panel1.add(combo);
+
+/////////////Date/////////////////////////////
+
+        //Adding the Date box to panel2
+        panel2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        label2 = new JLabel("Photos after ");
+
+        panel2.add(label2);
+
+
+////////////JList Title////////////////////////////
+
+        //Adding JList to panel3
+        panel3 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        jList = new JList<>();
+        jList.setPreferredSize(new Dimension(350, 200));
 
         //Listener for changing the values of the jList
         combo.addActionListener(new ActionListener() {
@@ -49,23 +72,6 @@ public class Mile4 extends JFrame {
             }
         });
 
-        panel1.add(label1);
-        panel1.add(combo);
-
-        //Adding the Date box to panel2
-        panel2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-
-        label2 = new JLabel("Photos after ");
-
-        panel2.add(label2);
-
-        //Adding JList to panel3
-        panel3 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-
-        jList = new JList<>();
-        jList.setPreferredSize(new Dimension(350, 200));
-
-
         //Adding the scroll bar to the JList
         scroll1 = new JScrollPane();
 
@@ -75,7 +81,7 @@ public class Mile4 extends JFrame {
 
         panel3.add(scroll1);
 
-
+///////////////JLabel Image///////////////////////////
 
         //Adding the JLabel with the ImageIcon to panel4
         panel4 = new JPanel(new FlowLayout(FlowLayout.CENTER));
@@ -84,11 +90,34 @@ public class Mile4 extends JFrame {
         icon = new ImageIcon("Mile4_Images/vangogh1.jpg");
         imageLabel.setIcon(icon);
 
+        /*
+        int index = combo.getSelectedIndex();
+        String[] picturePath = picturePath(index);
+
+        Photographer p1 = selectedPhotographer(index);
+        Picture picture1 =
+        if (p1.)
+
+         */
+        final String[] selectedTitle = {" "};
+        jList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2 && e.getButton() == MouseEvent.BUTTON1) {
+                    System.out.println("double clicked");
+                    selectedTitle[0] = jList.getSelectedValue();
+                    System.out.println(selectedTitle[0]);
+                }
+            }
+        });
+
         panel4.add(imageLabel);
 
         datePicker = new JXDatePicker();
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         panel2.add(datePicker);
+
+//////////////Add Panels/////////////////////////////
 
         //Adding panels to the container
         container.add(panel1);
@@ -149,4 +178,23 @@ public class Mile4 extends JFrame {
 
         return pictureNames;
     }
+
+    public String[] picturePath(int selectedIndex){
+        Photographer p1 = selectedPhotographer(selectedIndex);
+
+        List<Picture> pictureList = db.pictureList();
+
+        String [] picturePaths = new String[pictureList.size()];
+
+        int i=0;
+        for (Picture picture : pictureList){
+            if (p1.getId() == picture.getPhotographerId()){
+                picturePaths[i] = picture.getFile();
+            }
+            i++;
+        }
+
+        return picturePaths;
+    }
+
 }
